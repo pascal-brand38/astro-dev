13027b70627b15a04058fd9c974c3f332ad2cc2c
Pascal Brand <pascal.brand38@gmail.com>
2024-11-21 09:17:43 +0100

    astro-proto: multiple slides in one page
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- sites/astro-proto/astro.config.mjs
+++ sites/astro-proto/astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  compressHTML: false,
   vite: {
     css: {
         preprocessorOptions: {
