a7d4eeb58f337c7a2944ee479650a3bd39e6739a
Pascal Brand <pascal.brand38@gmail.com>
2024-11-25 17:14:02 +0100

    astro-dev-demo: http://localhost:4321/packages/astro-splide
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- sites/astro-dev-demo/astro.config.mjs
+++ sites/astro-dev-demo/astro.config.mjs
@@ -10,15 +10,19 @@ export default defineConfig({
 			social: {
 				github: 'https://github.com/pascal-brand38/astro-dev',
 			},
+			customCss: [
+        // Relative path to your custom CSS file
+        './src/styles/custom.css',
+      ],
 			tableOfContents: false,
 			sidebar: [
 				{
 					label: 'Packages',
-					autogenerate: { directory: 'packages' },
-					// items: [
-					// 	// Each item here is one entry in the navigation menu.
-					// 	{ label: 'Packages', slug: 'guides/example' },
-					// ],
+					// autogenerate: { directory: 'packages' },
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'astro-splide', slug: 'packages/astro-splide' },
+					],
 				},
 			],
 		}),
