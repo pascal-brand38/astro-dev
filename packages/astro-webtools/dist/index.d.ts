8eebd3d5bc56ce6fcbbe4e366fd9a5d095ffc7e7
Pascal Brand <pascal.brand38@gmail.com>
2024-12-11 10:37:19 +0100

    astro-webtools: all components in place
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- packages/astro-webtools/dist/index.d.ts
+++ packages/astro-webtools/dist/index.d.ts
@@ -14,3 +14,11 @@ export declare namespace webtools {
     }[];
     function createVarStyle(styleProps: string | astroHTML.JSX.CSSProperties | null, props: any, propsToVars: PropsToVars): string | astroHTML.JSX.CSSProperties | null;
 }
+
+// Copyright (c) Pascal Brand
+// MIT License
+
+export { default as Neon }  from './components/neon/Neon.astro';
+export { default as Polaroid }  from './components/polaroid/Polaroid.astro';
+export { default as Postit }  from './components/postit/Postit.astro';
+export { default as Garland }  from './components/garland/Garland.astro';
--- packages/astro-webtools/dist/index.js
+++ packages/astro-webtools/dist/index.js
@@ -25,3 +25,11 @@ export var webtools;
     }
     webtools.createVarStyle = createVarStyle;
 })(webtools || (webtools = {}));
+
+// Copyright (c) Pascal Brand
+// MIT License
+
+export { default as Neon }  from './components/neon/Neon.astro';
+export { default as Polaroid }  from './components/polaroid/Polaroid.astro';
+export { default as Postit }  from './components/postit/Postit.astro';
+export { default as Garland }  from './components/garland/Garland.astro';
