243c602b960a496b6abd3ebd29f87ed8512cf37e
Pascal Brand <pascal.brand38@gmail.com>
2024-12-11 09:30:25 +0100

    astro-webtools: namespace
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- packages/astro-webtools/dist/index.d.ts
+++ packages/astro-webtools/dist/index.d.ts
@@ -1,8 +1,16 @@
-export declare const WEBTOOLS_CONSTANTS: {
-    SECOND: string;
-    MINUTE: string;
-    HOUR: string;
-    YEAR: number;
-    MONTH: string;
-    DAY: string;
-};
+import 'astro/astro-jsx';
+export declare namespace webtools {
+    const CONSTANTS: {
+        SECOND: string;
+        MINUTE: string;
+        HOUR: string;
+        YEAR: number;
+        MONTH: string;
+        DAY: string;
+    };
+    type PropsToVars = {
+        var: string;
+        prop: string;
+    }[];
+    function createVarStyle(styleProps: string | astroHTML.JSX.CSSProperties | null, props: any, propsToVars: PropsToVars): string | astroHTML.JSX.CSSProperties | null;
+}
--- packages/astro-webtools/dist/index.js
+++ packages/astro-webtools/dist/index.js
@@ -1,12 +1,27 @@
 // Copyright (c) Pascal Brand
 // MIT License
-const now = new Date();
-const _digits2 = (number) => number < 10 ? '0' + number : '' + number;
-export const WEBTOOLS_CONSTANTS = {
-    SECOND: _digits2(now.getSeconds()),
-    MINUTE: _digits2(now.getMinutes()),
-    HOUR: _digits2(now.getHours()),
-    YEAR: now.getFullYear(),
-    MONTH: _digits2(now.getMonth() + 1),
-    DAY: _digits2(now.getDate()),
-};
+import 'astro/astro-jsx'; // import astroHTML namespace
+export var webtools;
+(function (webtools) {
+    const now = new Date();
+    const _digits2 = (number) => number < 10 ? '0' + number : '' + number;
+    webtools.CONSTANTS = {
+        SECOND: _digits2(now.getSeconds()),
+        MINUTE: _digits2(now.getMinutes()),
+        HOUR: _digits2(now.getHours()),
+        YEAR: now.getFullYear(),
+        MONTH: _digits2(now.getMonth() + 1),
+        DAY: _digits2(now.getDate()),
+    };
+    function createVarStyle(styleProps, props, propsToVars) {
+        let style = styleProps;
+        propsToVars.forEach((propToVar) => {
+            if (props[propToVar.prop]) {
+                style += `${propToVar.var}:${props[propToVar.prop]};`; // add css variable in style
+                delete props[propToVar.prop]; // rm this prop from all the astro props, so that we do not beg
+            }
+        });
+        return style;
+    }
+    webtools.createVarStyle = createVarStyle;
+})(webtools || (webtools = {}));
