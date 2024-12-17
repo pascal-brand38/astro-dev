05bcb5fd09d41ae75f42663df73eb25a8d9b3ae5
Pascal Brand <pascal.brand38@gmail.com>
2024-12-17 09:28:33 +0100

    astro-webtools: svgtext
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- packages/astro-webtools/src/components.d.ts
+++ packages/astro-webtools/src/components.d.ts
@@ -6,3 +6,4 @@ export { default as Polaroid }  from './components/polaroid/Polaroid.astro';
 export { default as Postit }  from './components/postit/Postit.astro';
 export { default as Garland }  from './components/garland/Garland.astro';
 export { default as Nav }  from './components/nav/Nav.astro';
+export { default as SvgText }  from './components/svgtext/SvgText.astro';
