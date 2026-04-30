cea66a4e6833956784f6990bae2543fb0d3f5087
Pascal Brand <pascal.brand38@gmail.com>
2026-04-30 17:10:40 +0200

    fix: starlight latest version
    Signed-off-by: Pascal Brand <pascal.brand38@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -21,9 +21,9 @@ export default defineConfig({
 		starlight({
 			title: 'astro-dev',
 			favicon: '/favicon.png',
-			social: {
-				github: 'https://github.com/pascal-brand38/astro-dev',
-			},
+			social: [
+				{ icon: 'github', label: 'GitHub', href: 'https://github.com/pascal-brand38/astro-dev' },
+			],
 			customCss: [
         // Relative path to your custom CSS file
         './src/styles/custom.css',
